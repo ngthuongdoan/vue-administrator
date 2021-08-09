f6151428928c9e221647e129e2da756895840edb
Doan Ngoc Thuong <ngthuongdoan@gmail.com>
2021-08-09 21:24:21 +0700

    feat: initial vue typescript

--- README.md
+++ README.md
@@ -1,34 +1,43 @@
 # vue-administrator
 
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Run your unit tests
+
 ```
 yarn test:unit
 ```
 
 ### Run your end-to-end tests
+
 ```
 yarn test:e2e
 ```
 
 ### Lints and fixes files
+
 ```
 yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <component :is="layout"></component>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import Default from '@/layouts/default.vue';
+
+@Component({
+  components: {
+    Default,
+  },
+})
+export default class App extends Vue {
+  layout = 'default';
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -15,18 +25,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
