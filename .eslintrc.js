4bdc67de857401a47d347098bc73daecd46d747f
ngthuongdoan <ngthuongdoan@gmail.com>
2021-08-11 13:32:10 +0700

    feat: add plop support
    plopjs uses for create file like angular cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,18 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true
   },
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    // "@vue/prettier/@typescript-eslint",
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
+  exclude: ["node_modules", "plop-templates"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
