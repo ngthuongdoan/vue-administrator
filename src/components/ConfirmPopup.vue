4a9a031905f4820ce2ca7f34a89e9fd29579ce89
ngthuongdoan <ngthuongdoan@gmail.com>
2021-08-17 20:09:55 +0700

    refactor: extract confirm popup component

---
+++ src/components/ConfirmPopup.vue
@@ -0,0 +1,24 @@
+<template>
+  <a-button-group>
+    <a-button @click="() => save(record.key)">Save</a-button>
+    <a-popconfirm title="Sure to cancel?" @confirm="() => cancel(record.key)">
+      <a-button type="danger">Cancel</a-button>
+    </a-popconfirm>
+  </a-button-group>
+</template>
+
+<script>
+export default {
+  props: ['record'],
+  methods: {
+    save(key) {
+      this.$emit('save', key);
+    },
+    cancel(key) {
+      this.$emit('cancel', key);
+    },
+  },
+};
+</script>
+
+<style></style>
