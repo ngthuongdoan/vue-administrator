51dced4900de6b546f9cd0ff2329557c7f36f33f
ngthuongdoan <ngthuongdoan@gmail.com>
2021-08-26 18:04:21 +0700

    feat: mock jwt

--- mock/index.js
+++ mock/index.js
@@ -1,10 +1,116 @@
-const category = require('./routes/category');
+const bodyParser = require('body-parser');
+const jsonServer = require('json-server');
+const jwt = require('jsonwebtoken');
 const user = require('./routes/user');
-const { deletePassword } = require('./utils');
-module.exports = () => {
-  user.forEach((u) => deletePassword(u));
-  return {
-    category,
-    user,
-  };
-};
+const category = require('./routes/category');
+const { generateId } = require('./utils');
+
+const server = jsonServer.create();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(jsonServer.defaults());
+
+const SECRET_KEY = '123456789';
+
+const expiresIn = '1h';
+
+// Create a token from a payload
+function createToken(payload) {
+  return jwt.sign(payload, SECRET_KEY, { expiresIn });
+}
+
+// Verify the token
+function verifyToken(token) {
+  return jwt.verify(token, SECRET_KEY, (err, decode) =>
+    decode !== undefined ? decode : err
+  );
+}
+
+// Check if the user exists in database
+function isAuthenticated({ username, password }) {
+  return (
+    user.findIndex(
+      (u) => u.username === username && u.password === password
+    ) !== -1
+  );
+}
+
+// Register New User
+server.post('/auth/register', (req, res) => {
+  console.log('register endpoint called; request body:');
+  console.log(req.body);
+  const { username, password, fullname, phone, address, role } = req.body;
+
+  if (isAuthenticated({ username, password })) {
+    const status = 401;
+    const message = 'Username and Password already exist';
+    res.status(status).json({ status, message });
+    return;
+  }
+  //Add new user
+  user.push({
+    id: generateId(),
+    username,
+    password,
+    fullname,
+    phone,
+    address,
+    role,
+  });
+  // Create token for new user
+  const access_token = createToken({ username, password });
+  console.log('Access Token:' + access_token);
+  res.status(200).json({ token: access_token });
+});
+
+// Login to one of the users from ./users.json
+server.post('/auth/login', (req, res) => {
+  console.log('login endpoint called; request body:');
+  console.log(req.body);
+  const { username, password } = req.body;
+  if (isAuthenticated({ username, password }) === false) {
+    const status = 401;
+    const message = 'Incorrect username or password';
+    res.status(status).json({ status, message });
+    return;
+  }
+  const access_token = createToken({ username, password });
+  console.log('Access Token:' + access_token);
+  res.status(200).json({ access_token });
+});
+
+server.use(/^(?!\/auth).*$/, (req, res, next) => {
+  if (
+    req.headers.authorization === undefined ||
+    req.headers.authorization.split(' ')[0] !== 'Bearer'
+  ) {
+    const status = 401;
+    const message = 'Error in authorization format';
+    res.status(status).json({ status, message });
+    return;
+  }
+  try {
+    let verifyTokenResult;
+    verifyTokenResult = verifyToken(req.headers.authorization.split(' ')[1]);
+
+    if (verifyTokenResult instanceof Error) {
+      const status = 401;
+      const message = 'Access token not provided';
+      res.status(status).json({ status, message });
+      return;
+    }
+    next();
+  } catch (err) {
+    const status = 401;
+    const message = 'Error access_token is revoked';
+    res.status(status).json({ status, message });
+  }
+});
+
+server.use(jsonServer.router({ category }));
+
+server.listen(8000, () => {
+  console.log('Run Auth API Server');
+  console.log('http://localhost:8000');
+});
